f566791b23807d1df4226a61cd7a2cc09dbffac9
Elias Hanken Fiskerstrand <streafe@gmail.com>
2026-06-04 21:07:49 +0200

    chore: untrack tmp/ scratch dir + gitignore it (accidental inclusion in #78 merge) (#79)

--- tmp/build_and_run.bat
+++
@@ -1,3 +0,0 @@
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-cl /nologo /std:c++17 /EHsc "C:\Users\elias\Documents\_dev\iron-core-engine\tmp\offset_check.cpp" /Fe"C:\Users\elias\Documents\_dev\iron-core-engine\tmp\offset_check.exe" /Fo"C:\Users\elias\Documents\_dev\iron-core-engine\tmp\offset_check.obj"
-"C:\Users\elias\Documents\_dev\iron-core-engine\tmp\offset_check.exe"
